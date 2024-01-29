e26184bb6b0a3e670fa737251f29bb210d5fed89
João Valdir <joaovaldir21@gmail.com>
2024-01-29 16:55:12 -0300

    Pagina inicial de Produtos

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
 {
     
-    public function index () {
-        return 'produtos';
+    public function index () 
+    {
+        $findProdutos = Produto::all();
+        return view('pages.produtos.paginacao', compact('findProdutos'));
     }
 
 
